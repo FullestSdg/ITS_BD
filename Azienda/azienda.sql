a9f101f631031f8cbeb49942cff536950de99775
FullestSdg <sdgsergiodg20@gmail.com>
2025-07-16 16:59:50 +0200

    updated

---
+++ Azienda/azienda.sql
@@ -0,0 +1,35 @@
+create domain RealGEZ as real 
+    check (value >= 0);
+
+create domain Stringa as varchar;
+
+create type Indirizzo as (
+    via Stringa,
+    civico Stringa
+); 
+
+create table Impiegato(
+    id integer primary key,
+    nome Stringa not null,
+    cognome Stringa not null,
+    nascita date not null,
+    stipendio RealGEZ not null 
+);
+
+create table Dipartimento(
+    id integer primary key,
+    nome Stringa not null,
+    indirizzo Indirizzo
+);
+
+create table Progetto(
+    id integer primary key,
+    nome Stringa not null,
+    budget RealGEZ not null
+);
+
+create table NumeroTelefono(
+    telefono Stringa primary key 
+
+    unique (telefono)
+);
