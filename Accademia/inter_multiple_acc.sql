c8e011ce46f78f794e12ae0063e8ebb5da6e731c
FullestSdg <sdgsergiodg20@gmail.com>
2025-07-21 20:22:38 +0200

    updated

--- Accademia/inter_multiple_acc.sql
+++ Accademia/inter_multiple_acc.sql
@@ -6,7 +6,15 @@ where progetto.nome = 'Pegasus';
 
 -- 2. Quali sono il nome, il cognome e la posizione degli strutturati che hanno almeno una attività nel progetto ‘Pegasus’, ordinati per cognome decrescente?
 
-select persona 
-from AttivitaProgetto.progetto
-where progetto.nome = 'Pegasus'
-order by persona.cognome;
+select distinct p.nome, p.cognome, p.posizione
+from Persona as p, Progetto as pr, AttivitaProgetto as a1, AttivitaProgetto as a2
+where pr.id = a2.progetto and a1.persona = p.id and a2.persona = p.id and a1.progetto = pr.id and a1.id <> a2.id and pr.nome = 'Pegasus';
+
+-- 3. Quali sono il nome, il cognome e la posizione degli strutturati che hanno più di una attività nel progetto ‘Pegasus’?
+
+select distinct p.nome, p.cognome, p.posizione
+from Persona as p, Progetto as pr, AttivitaProgetto as ap
+where pr.nome = 'Pegasus'
+
+-- Mi sono arreso, chiedo scusa :(
+
