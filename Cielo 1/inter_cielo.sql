b5f7cd4c62bdefdfe865f954362e95836392b054
FullestSdg <sdgsergiodg20@gmail.com>
2025-07-22 11:34:34 +0200

    updated

--- Cielo 1/inter_cielo.sql
+++ Cielo 1/inter_cielo.sql
@@ -28,23 +28,36 @@ where ArrPart.arrivo = 'FCO';
 
 select codice, comp 
 from ArrPart 
-where ArrPart.partenza = 'FCO' AND ArrPart.arrivo = 'JFK';
+where ArrPart.partenza = 'FCO' 
+    and ArrPart.arrivo = 'JFK';
 
 -- 6. Quali sono le compagnie che hanno voli che partono dall’aeroporto ‘FCO’ e atterrano all’aeroporto ‘JFK’?
 
 select comp 
 from ArrPart 
-where ArrPart.partenza = 'FCO' AND ArrPart.arrivo = 'JFK';
+where ArrPart.partenza = 'FCO' 
+    and ArrPart.arrivo = 'JFK';
 
 -- 7. Quali sono i nomi delle compagnie che hanno voli diretti dalla città di ‘Roma’ alla città di ‘New York’?
 
-
-
+select distinct comp
+from ArrPart ap, luogoaeroporto lap, luogoaeroporto laa 
+where lap.aeroporto = ap.partenza 
+    and laa.aeroporto = ap.arrivo
+    and lap.citta = 'Roma'
+    and laa.citta = 'New York'
 
 -- 8. Quali sono gli aeroporti (con codice IATA, nome e luogo) nei quali partono voli della compagnia di nome ‘MagicFly’?
 
 
 
 
--- 9 9. Quali sono i voli che partono da un qualunque aeroporto della città di ‘Roma’ e atterrano ad un qualunque aeroporto della città di ‘New York’? Restituire: codice del volo, nome della compagnia, e aeroporti di partenza e arrivo.
+-- 9. Quali sono i voli che partono da un qualunque aeroporto della città di ‘Roma’ e atterrano ad un qualunque aeroporto della città di ‘New York’? Restituire: codice del volo, nome della compagnia, e aeroporti di partenza e arrivo.
 
+
+-- 11
+
+select * 
+from ArrPart ao, compagnia c 
+where ap.comp = c.nom 
+    and c.annoFondaz is not null;
