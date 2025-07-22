8300910e65cf6febedddebfb6eb0a97025f1ef22
FullestSdg <sdgsergiodg20@gmail.com>
2025-07-22 09:22:24 +0200

    updated

--- Accademia/inter_multiple_acc.sql
+++ Accademia/inter_multiple_acc.sql
@@ -4,17 +4,38 @@ select nome, inizio, fine
 from WP.progetto
 where progetto.nome = 'Pegasus';
 
+
+-- versione giusta sotto
+
+select *
+from progetto p, wp 
+where wp.progetto = p.id
+    and p.nome = 'Pegasus';
+
 -- 2. Quali sono il nome, il cognome e la posizione degli strutturati che hanno almeno una attività nel progetto ‘Pegasus’, ordinati per cognome decrescente?
 
-select distinct p.nome, p.cognome, p.posizione
-from Persona as p, Progetto as pr, AttivitaProgetto as a1, AttivitaProgetto as a2
-where pr.id = a2.progetto and a1.persona = p.id and a2.persona = p.id and a1.progetto = pr.id and a1.id <> a2.id and pr.nome = 'Pegasus';
+select pers.nome, pers.cognome, pers.posizione
+from persona pers, progetto prog, attivitaprogetto ap
+where ap.persona = pers.id 
+    and ap.progetto = prog.id;
+    and prog.nome = 'Pegasus'
+order by pers.cognome desc;
 
--- 3. Quali sono il nome, il cognome e la posizione degli strutturati che hanno più di una attività nel progetto ‘Pegasus’?
+-- 3. Quali sono il nome, il cognome e la posizione degli strutturati che hanno più di una attività nel progetto ‘Pegasus’ ?
 
-select distinct p.nome, p.cognome, p.posizione
-from Persona as p, Progetto as pr, AttivitaProgetto as ap
-where pr.nome = 'Pegasus'
+select distinct pers.nome, pers.cognome, pers.posizione
+from persona pers, progetto prog, attivitaprogetto ap1, attivitaprogetto ap2
+where prog.nome = 'Pegasus'
+    and ap1.persona = pers.id
+    and ap2.persona = pers.id
+    and ap1.progetto = prog.id 
+    and ap2.progetto = prog.id
+    and ap1.id <> ap2.id;
 
--- Mi sono arreso, chiedo scusa :(
+-- 8. Quali sono il nome e il cognome degli strutturati che nello stesso giorno hanno sia attività progettuali che attività non progettuali?
 
+select distinct p.nome, p.cognome
+from persona p, attivitaprogetto ap, attivitanonprogettuale anp;
+where ap.giorno = anp.giorno 
+    and ap.persona = p.id 
+    and anp.persona = p.id;
