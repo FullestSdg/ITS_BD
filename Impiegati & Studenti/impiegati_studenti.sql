0a14b88dfe230825377fcf9d55676ee4c81b11dd
FullestSdg <sdgsergiodg20@gmail.com>
2025-08-18 16:43:06 +0200

    updated

--- Impiegati & Studenti/impiegati_studenti.sql
+++ Impiegati & Studenti/impiegati_studenti.sql
@@ -20,42 +20,74 @@ create table Persona (
 );
 
 create table Uomo (
+    cf CodiceFiscale primary key,
+
+    foreign key (cf) references Persona(cf)
 
 );
 
 create table Donna (
-    maternita IntGEZ not null
+    maternita IntGEZ not null,
+    cf CodiceFiscale primary key,
+
+    foreign key (cf) references Persona(cf)
 );
 
 create table PosizioneMilitare (
-    nome Stringa primary key
+    nome Stringa primary key,
+
+    foreign key (nome) references Uomo(cf)
 );
 
 create table Impiegato (
+    cf CodiceFiscale primary key,
+    stipendio RealGZ not null,
 
+    foreign key (cf) references Persona(cf)
 );
 
 create table Studente (
+    cf CodiceFiscale primary key,
+    matricola IntGZ not null,
 
+    foreign key (cf) references Persona(cf)
 );
 
 create table Direttore (
+    cf CodiceFiscale primary key,
+    stipendio RealGZ not null,
 
+    foreign key (cf) references Persona(cf),
+    foreign key (stipendio) references Impiegato(stipendio)
 );
 
 create table Segretario (
+    cf CodiceFiscale primary key,
+    stipendio RealGZ not null,
 
+    foreign key (cf) references Persona(cf),
+    foreign key (stipendio) references Impiegato(stipendio)
 );
 
 create table Progettista (
+    cf CodiceFiscale primary key,
+    stipendio RealGZ not null,
 
+    foreign key (cf) references Persona(cf),
+    foreign key (stipendio) references Impiegato(stipendio)
 );
 
 create table Reponsabile (
+    cf CodiceFiscale primary key,
+    stipendio RealGZ not null,
 
+    foreign key (cf) references Persona(cf),
+    foreign key (stipendio) references Impiegato(stipendio)
 );
 
 create table Progetto (
     nome varchar not null,
     id integer primary key
+
+    foreign key (id) references Responsabile(cf)
 ); 
