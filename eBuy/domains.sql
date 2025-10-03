c09628f27a548106d0e6e290b46284fff78bc710
FullestSdg <sdgsergiodg20@gmail.com>
2025-10-03 17:42:24 +0200

    updated

--- eBuy/domains.sql
+++ eBuy/domains.sql
@@ -15,18 +15,7 @@ create domain IntGEZ as integer
 create domain IntGE2 as integer 
     check (value >= 2);
 
-create domain DataOra as datetime;
-
 create domain URL as text; -- https://stackoverflow.com/questions/219569/best-database-field-type-for-a-url
 
 create type Condizione as 
     enum ('');
-
-create type TipoUtente as 
-    enum ('Privato', 'VenditoreProf');
-
-create type TipoPostOggetto as
-    enum ('PostOggettoAsta', 'PostOggettoCompraloSubito');
-
-create type TipoPostOggetto2 as 
-    enum ('PostOggettoNuovo', 'PostOggettoUsato');
